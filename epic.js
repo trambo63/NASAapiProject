847444ead0ce06fd3c22a1a851d4f737d7e7d03a
Tom <tomrambo95@yahoo.com>
2021-02-17 09:19:27 -0500

    slideshow WIP

--- epic.js
+++ epic.js
@@ -2,6 +2,7 @@ const key = 'r2jRbdbVKRy6mnkRnkRP8YgBfg1DKYgZq9n5vIoz';
 const epicURL = `https://api.nasa.gov/EPIC/api/natural/?api_key=${key}`;
 const innerCarousel = document.querySelector('#main-carousel');
 
+
 fetchEpic();
 
 function fetchEpic(){
@@ -50,4 +51,57 @@ function displayMain(json){
     });
 }
 
+// Image Search
+const searchByDate = document.querySelector('form');
+const searchInnerCarousel = document.querySelector('#search-carousel');
 
+searchByDate.addEventListener('submit', fetchImgByDate);
+
+function fetchImgByDate(e){
+    e.preventDefault();
+    console.log('in fetch');
+    let dateSearchURL = `https://api.nasa.gov/EPIC/api/natural/date/${date.value}?api_key=${key}`;
+    console.log(dateSearchURL);
+    
+    fetch(dateSearchURL) //fetch runs asyncronously and we are passing in the url
+    .then(function(result) {    
+    return result.json(); 
+    }).then(function(json) {
+        console.log(json);
+        displayImgByDate(json);
+    });
+}
+
+function displayImgByDate(json){
+    console.log('in display function');
+    json.forEach(function(json){
+        let mainImg = json.image;
+        let date = json.date;
+        let imgDateTime = date.split(" ");
+        let imgDate = imgDateTime[0].split('-');
+        let imgYear = imgDate[0];
+        let imgMonth = imgDate[1];
+        let imgDay = imgDate[2];
+        let imgURL = `https://api.nasa.gov/EPIC/archive/natural/${imgYear}/${imgMonth}/${imgDay}/png/${mainImg}.png?api_key=${key}`
+        console.log(imgURL);
+        
+        let carouselItem = document.createElement('div');
+        carouselItem.classList.add('carousel-item');
+        //carouselItem.classList.add('active');
+        let carouselImg = document.createElement('img');
+        carouselImg.classList.add('d-block');
+        carouselImg.classList.add('w-100');
+        carouselImg.src = imgURL;
+
+        let dateCaption = document.createElement('p');
+        dateCaption.textContent = json.date;
+        
+        let caption = document.createElement('p');
+        caption.textContent = json.caption;
+
+        searchInnerCarousel.appendChild(carouselItem);
+        carouselItem.appendChild(dateCaption);
+        carouselItem.appendChild(carouselImg);
+        carouselItem.appendChild(caption);
+    });
+}
